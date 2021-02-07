7ba7a9ae74b5a91e70231479aad8f9e6eaf5a0df
antoinebrunet97 <antoinebrunet97@outlook.com>
2021-02-07 10:44:10 -0500

    testing app

--- public/assets/js/resultsfromsearch.js
+++ public/assets/js/resultsfromsearch.js
@@ -1,7 +1,10 @@
 let map;
+// array of markers
+let markers = [];
+// indicates whether all the markers appear on the map
+let markersIsFull = true;
 let places;
 let infoWindow;
-let markers = [];
 let autocomplete;
 let placeTwo;
 let searchImage;
@@ -15,8 +18,8 @@ const hostnameRegexp = new RegExp("^https?://.+?/");
 const countries = {
   address: {
     // Initial coordinates of the map
-    center: { lat: 40.7128, lng: -74.006 },
-    zoom: 12,
+    center: { lat: 45.508888, lng: -73.561668 },
+    zoom: 11,
   },
 };
 
@@ -27,29 +30,79 @@ function initMap() {
     center: countries["address"].center,
     mapTypeControl: false,
     panControl: false,
-    zoomControl: false,
+    zoomControl: true,
     streetViewControl: false,
     styles: mapstyling,
   });
 
-  // For loop of all recycling bins
-  for (i = 0; i < recyclingbin.length; i++) {
-    latVal = recyclingbin[i].Latitude;
-    lngVal = recyclingbin[i].Longitude;
+  // For loop of all libraries
+  for (i = 0; i < libraries.length; i++) {
+    latVal = libraries[i].latitude;
+    lngVal = libraries[i].longitude;
     latlng = { lat: latVal, lng: lngVal };
-    console.log(latlng);
-    new google.maps.Marker({
+    libraryName = libraries[i].name;
+    addressPart1 = libraries[i].addressPart1;
+    addressPart2 = libraries[i].addressPart2;
+    phoneNumberPart1 = libraries[i].phoneNumberPart1;
+    phoneNumberPart2 = libraries[i].phoneNumberPart2;
+    let contentString =
+    '<p>' + libraryName + '</p>' +
+    '<p>Adresse:</p>' +
+    '<p>' + addressPart1 + '</p>' +
+    '<p>' + addressPart2 + '</p>' +
+    '<p>Téléphone:</p>' +
+    '<p>' + phoneNumberPart1 + '</p>';
+    if (!(phoneNumberPart2 === "")) {
+      contentString += '<p>' + phoneNumberPart2 + '</p>';
+    }
+    // info window
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+    // marker
+    const marker = new google.maps.Marker({
       position: latlng,
       map,
-      title: "Recycle Bin #" + i,
+      // title: libraryName,
       clickable: true,
     });
+    // the info window opens on click
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+    markers.push(marker)
   }
 
-  searchMarker = new google.maps.Marker({
-    position: { lat: 40.85557, lng: -73.887565 },
-    map,
-    title: "Recycle Bin #1",
-    clickable: true,
-  });
+  // updates the markers
+  function updateMarkers() {
+    // neighborhood
+    neighborhood = document.getElementById("neighborhood").value;
+    if (neighborhood === "All neighborhoods") {
+      // makes all the markers appear
+      for (let i = 0; i < libraries.length; i++) {
+          markers[i].setMap(map);
+          markersIsFull = true;
+      }
+    } else {
+      if (!markersIsFull) {
+        // makes the appropriate markers appear
+        for (let i = 0; i < libraries.length; i++) {
+          libraryName = libraries[i].neighborhood;
+          if (libraryName === neighborhood) {
+            markers[i].setMap(map);
+          }
+        }
+      }
+      // makes the other markers disappear
+      for (let i = 0; i < libraries.length; i++) {
+        libraryName = libraries[i].neighborhood;
+        if (!(libraryName === neighborhood)) {
+          markers[i].setMap(null);
+          markersIsFull = false;
+        }
+      }
+    }
+  }
+
+  document.getElementById("neighborhood").onchange=updateMarkers;
 }
